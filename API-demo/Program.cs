5005419381065084e6b394b8813795525dcb924d
Vira Murray <vira.avramenko@gmail.com>
2022-10-12 10:42:49 -0400

    initial commit

---
+++ API-demo/Program.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+var client = new RestClient("https://free-football-soccer-videos.p.rapidapi.com/");
+var request = new RestRequest();
+request.AddHeader("X-RapidAPI-Key", "468f7462bdmsh9e2bcd97b71b95cp139dc0jsnedb83d6f5cc5");
+request.AddHeader("X-RapidAPI-Host", "free-football-soccer-videos.p.rapidapi.com");
+var response = client.Execute(request);
+
+Console.WriteLine(response.Content);
